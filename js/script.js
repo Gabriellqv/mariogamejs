93981c071e4e5208dc74d2769b84803656e8d321
Gabriellqv <gabriellqv@gmail.com>
2023-09-26 20:05:58 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const mario = document.querySelector('.mario');
+const mario2 = document.querySelector('.mario2');
 const pipe = document.querySelector('.pipe');
+const jumpSound = new Audio('./sounds/mario-jump.mp3');
+const deathSound = new Audio('./sounds/mario-death.mp3');
+const songSound = new Audio('./sounds/mario-song.mp3');
 
 const jump  = () => {
-    mario.classList.add('jump');
 
+    mario.classList.add('jump');
+    mario.src ='./images/marioJump.png';
+    mario.style.width = '85px';
+    mario.style.marginLeft = '50px';
+
+    jumpSound.play();
     setTimeout(() => {
         
         mario.classList.remove('jump');
+        mario.src ='./images/mario.gif';
+        mario.style.width = '150px';
+        mario.style.marginLeft = '5.7px';
 
     }, 500);
+
 }
 
 const loop = setInterval(() => {
 
+    songSound.play();
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
@@ -24,7 +38,8 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-
+        songSound.pause();
+        deathSound.play();
         mario.src ='./images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
